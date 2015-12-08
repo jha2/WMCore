67a59a15c9576e0942cf809d554b94db642a094e
Manoj <Manoj.Jha@cern.ch>
2015-12-09 00:17:10 +0100

    Added unittests using mock for all the members available in json file ...

--- test/python/WMQuality_t/Emulators_t/DBSClient_t/MockDbsApi_t.py
+++ test/python/WMQuality_t/Emulators_t/DBSClient_t/MockDbsApi_t.py
@@ -39,6 +39,45 @@ class MockDbsApiTest(unittest.TestCase):
         self.assertEqual(sorted(results), sorted(mock))
         return
 
+    def testListDatasets(self):
+        """
+        listDatasets returns known  datasets for a given data_tier_name and primary ds
+        """
+        results = self.realDBS.listDatasets(data_tier_name = 'RAW', primary_ds_name = 'Jet') 
+        mock = self.mockDBS.listDatasets(data_tier_name = 'RAW', primary_ds_name = 'Jet')
+        #assert the result
+        self.assertEqual(sorted(results), sorted(mock))
+        return
+    
+    def testListFileLumis(self):
+        """
+        listFileLumis returns known lumi sections  for a given  block
+        """
+        results = self.realDBS.listFileLumis(block_name = BLOCK, validFileOnly = 1) 
+        mock = self.mockDBS.listFileLumis(block_name = BLOCK, validFileOnly = 1) 
+        #assert the result
+        self.assertEqual(sorted(results), sorted(mock))
+        return
+    
+    def testListFileLumiArray(self):
+        """
+        listFileLumiArray returns known lumi sections  for a given list of files
+        """
+        file_names = [u'/store/data/Commissioning2015/Cosmics/RAW/v1/000/238/545/00000/C47FDF25-2ECF-E411-A8E2-02163E011839.root', u'/store/data/Commissioning2015/Cosmics/RAW/v1/000/238/545/00000/04FBE4D8-2DCF-E411-B827-02163E0124D5.root', u'/store/data/Commissioning2015/Cosmics/RAW/v1/000/238/545/00000/1043E89F-2DCF-E411-9CAE-02163E013751.root', u'/store/data/Commissioning2015/Cosmics/RAW/v1/000/238/545/00000/FA4E40C0-2DCF-E411-AD1A-02163E012186.root']
+        results = self.realDBS.listFileLumiArray(logical_file_name = file_names) 
+        mock = self.mockDBS.listFileLumiArray(logical_file_name = file_names) 
+        #assert the result
+        self.assertEqual(sorted(results), sorted(mock))
+        return
+    
+    def testlistFileParents(self):
+        """listFileParents returns parent logical and logical file names  in a block"""
+        results = self.realDBS.listFileParents(block_name = "/Cosmics/Commissioning2015-PromptReco-v1/RECO#004ac3ba-d09e-11e4-afad-001e67ac06a0") 
+        mock = self.mockDBS.listFileParents(block_name = "/Cosmics/Commissioning2015-PromptReco-v1/RECO#004ac3ba-d09e-11e4-afad-001e67ac06a0") 
+        #assert the result
+        self.assertEqual(sorted(results), sorted(mock))
+        return 
+        
     def testListPrimaryDatasets(self):
         """
         listPrimaryDatasets returns known primary datasets
@@ -86,62 +125,25 @@ class MockDbsApiTest(unittest.TestCase):
         self.assertEqual(sorted(results), sorted(mock))
         return
 
-#    def testGetFileBlocksInfo(self):
-#        """getFileBlocksInfo returns block info, including location lookup"""
-#        # results = self.realDBS.getFileBlocksInfo(dataset = DATASET) 
-#        mock = self.mockDBS.getFileBlocksInfo(dataset = DATASET)
-#        print "real: %s" %results
-        
+    def testGetFileBlocksInfo(self):
+        """getFileBlocksInfo returns block info, including location lookup"""
+        results = self.realDBS.listBlocks(dataset = DATASET, detail = True) 
+        mock = self.mockDBS.listBlocks(dataset = DATASET, detail = True) 
         #assert the result
-        #self.assertEqual(sorted(results), sorted(mock))
-#        self.dbs = DBSReader(self.endpoint)
-#        blocks = self.mockDBS.getFileBlocksInfo(DATASET)
-#        block = self.mockDBS.getFileBlocksInfo(DATASET, blockName=BLOCK)
-#        self.assertEqual(1, len(block))
-#        block = block[0]
-#        self.assertEqual(46, len(blocks))
-#        self.assertTrue(block['Name'] in [x['Name'] for x in blocks])
-#        self.assertEqual(BLOCK, block['Name'])
-#        self.assertEqual(0, block['OpenForWriting'])
-#        self.assertEqual(150780132, block['BlockSize'])
-#        self.assertEqual(2, block['NumberOfFiles'])
-#        # possibly fragile but assume block located at least at cern
-#        sites = [x['Name'] for x in block['PhEDExNodeList'] if x['Name'].find('CH_CERN') > -1]
-#        self.assertTrue(sites)
-#
-#        # weird error handling - depends on whether block or dataset is missing
-#        self.assertRaises(DBSReaderError, self.mockDBS.getFileBlocksInfo, DATASET + 'blah')
-#        self.assertRaises(DBSReaderError, self.mockDBS.getFileBlocksInfo, DATASET, blockName=BLOCK + 'asas')
-#
-#    def testListFileBlocks(self):
-#        """listFileBlocks returns block names in dataset"""
-#        self.dbs = DBSReader(self.endpoint)
-#        blocks = self.mockDBS.listFileBlocks(DATASET)
-#        self.assertTrue(BLOCK in blocks)
-#        # block is closed
-#        block = self.mockDBS.listFileBlocks(DATASET, blockName=BLOCK, onlyClosedBlocks=True)[0]
-#        self.assertEqual(block, BLOCK)
-#        self.assertTrue(BLOCK in block)
-#
-#    def testListOpenFileBlocks(self):
-#        """listOpenFileBlocks finds open blocks"""
-#        # hard to find a dataset with open blocks, so don't bother
-#        self.dbs = DBSReader(self.endpoint)
-#        self.assertFalse(self.mockDBS.listOpenFileBlocks(DATASET))
-#
-#    def testBlockExists(self):
-#        """blockExists returns existence of blocks"""
-#        self.dbs = DBSReader(self.endpoint)
-#        self.assertTrue(self.mockDBS.blockExists(BLOCK))
-#        self.assertRaises(DBSReaderError, self.mockDBS.blockExists, DATASET + '#somethingelse')
-#
-#    def testListFilesInBlock(self):
-#        """listFilesInBlock returns files in block"""
-#        self.dbs = DBSReader(self.endpoint)
-#        self.assertTrue(FILE in [x['LogicalFileName'] for x in self.mockDBS.listFilesInBlock(BLOCK)])
-#        self.assertRaises(DBSReaderError, self.mockDBS.listFilesInBlock, DATASET + '#blah')
+        self.assertEqual(sorted(results), sorted(mock))
 
+    def testListFilesInBlockWithParents(self):
+        """ get parents for a block"""
+        results = self.realDBS.listBlockParents(block_name = '/Cosmics/Commissioning2015-PromptReco-v1/RECO#004ac3ba-d09e-11e4-afad-001e67ac06a0')
+        mock = self.mockDBS.listBlockParents(block_name = '/Cosmics/Commissioning2015-PromptReco-v1/RECO#004ac3ba-d09e-11e4-afad-001e67ac06a0')
+        #assert the result
+        self.assertEqual(sorted(results), sorted(mock))
 
+        """gets files with parents for a block"""
+        results = self.realDBS.listFileArray(block_name = '/Cosmics/Commissioning2015-PromptReco-v1/RECO#004ac3ba-d09e-11e4-afad-001e67ac06a0', detail = True, validFileOnly = 1)
+        mock = self.mockDBS.listFileArray(block_name = '/Cosmics/Commissioning2015-PromptReco-v1/RECO#004ac3ba-d09e-11e4-afad-001e67ac06a0', detail = True, validFileOnly = 1)
+        #assert the result
+        self.assertEqual(sorted(results), sorted(mock))
 
 if __name__ == '__main__':
     unittest.main()
